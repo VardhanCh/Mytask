a1d4a331f8fe1fe8915c28a6a3b958af3eddc61c
VardhanCh <101689386+VardhanCh@users.noreply.github.com>
2022-04-15 22:41:42 +0530

    Add files via upload

---
+++ Day 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleAppCoreDayfourtask1.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCoreDayfourtask1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<RegDet> dlist = new List<RegDet>();
+            var httpclient = new HttpClient();
+            var response = await httpclient.GetAsync("https://localhost:44373/api/Regd");
+            string result = await response.Content.ReadAsStringAsync();
+            dlist = JsonConvert.DeserializeObject<List<RegDet>>(result);
+
+            foreach (var i in dlist)
+            {
+                Console.WriteLine($"Name:{i.FirstName}\tRID:{i.Rid}\tMAIL ID:{i.Mailid}\tCONTACTNO{i.Contactno}\tSKILLSET:{i.Skillset}");
+            }
+        }
+    }
+}
